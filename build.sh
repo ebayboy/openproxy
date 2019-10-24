56d496445043fe44727de86f75dd0cb62606e166
fanpengfei <fanpengfei@jd.com>
2019-10-24 15:09:03 +0800

    fw

--- build.sh
+++ build.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 INSTALL_PATH=/usr/local/myresty
+BUILDROOT=./build
+OPENRESTY_VERSION=v1.15.8.2
+
+git submodule foreach git pull
+
+exit 0;
+
+#checkout code
+git checkout -b v1.15.8.2 v1.15.8.2
+
+rf -rf $BUILDROOT &&  mkdir $BUILDROOT
+
+#copy code
+cp -afR vendors/openresty/ $BUILDROOT
+cp -afR 3Party/ $BUILDROOT
+
+cd $BUILDROOT
 
 ./configure \
     --prefix=/usr/local/myresty \
@@ -21,9 +38,12 @@ INSTALL_PATH=/usr/local/myresty
 gmake && gmake install
 
 #install my lua_modules
-cd 3Party/lua_modules 
+cd ./3Party/lua_modules 
 sh ./build_lua_modules.sh  
 cp lib/* /usr/local/myresty/lualib/ -R
 cd -
 
+cd ../
+
 #tar 
+
